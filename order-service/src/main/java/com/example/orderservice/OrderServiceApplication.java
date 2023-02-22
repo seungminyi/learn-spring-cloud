ddf4dd1171cf4c152c263c8f678335cca3d79702
seungminyi <seungminyi95@gmail.com>
2023-02-22 18:01:36 +0900

    use gateway filter and predicates

--- order-service/src/main/java/com/example/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/example/orderservice/OrderServiceApplication.java
@@ -1,15 +1,23 @@
 package com.example.orderservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableEurekaClient
 @SpringBootApplication
+@RestController
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OrderServiceApplication.class, args);
     }
 
+    @GetMapping("/info")
+    public String info(@Value("${server.port}") String port) {
+        return "Order 서비스의 기본 동작 Port: {" + port + "}";
+    }
 }
--- user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -1,15 +1,29 @@
 package com.example.userservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RestController
 public class UserServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
     }
 
+    @GetMapping("/info")
+    public String info(@Value("${server.port}") String port) {
+        return "User 서비스의 기본 동작 Port: {" + port + "}";
+    }
+
+    @GetMapping("/auth")
+    public String auth(@RequestHeader(value = "token") String token) {
+        return "token is " + token;
+    }
 }
